653e53f845c9caf144a3d6f8292cb5f3c35c5a20
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-02-12 21:38:57 +0100

    Developed test for CitySearch

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme/build';
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
 // describe - is a container for scope of tests
 describe('<App /> component', () => {
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
   test('render list of events', () => {
-  const AppWrapper = shallow(<App />);
-  expect(AppWrapper.find(EventList)).toHaveLength(1);
-});
+    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
 });
