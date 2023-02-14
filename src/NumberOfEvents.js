b2e00fa664b9ecf40ed9d8bace97cbec6372790d
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-02-14 21:24:13 +0100

    Created tests for NumberOfEvents

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
+  state = {
+    noe: 36,
+  };
+
+  changeNOE(value) {
+    this.setState({ noe: value });
+  }
+
   render() {
-    return <div className='NumberOfEvents'></div>;
+    const { noe } = this.state;
+    return (
+      <div className='NumberOfEvents'>
+        <input
+          list='noe'
+          type='number'
+          className='noe-input'
+          value={noe}
+          onChange={(event) => {
+            this.changeNOE(event.target.value);
+          }}
+        />
+        <datalist id='noe'>
+          <option value='36' />
+          <option value='72' />
+          <option value='108' />
+        </datalist>
+      </div>
+    );
   }
 }
 
