6957cb791413c3d308ee6c06355a0c30d464e3f2
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-02-14 02:05:40 +0100

    Developed tests for collapsed state EventDetails

--- src/Event.js
+++ src/Event.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import EventDetails from './EventDetails';
 
 class Event extends Component {
+  state = {
+    collapsed: true,
+  };
+
+  toogleState = () => {
+    this.setState({
+      collapsed: false,
+    });
+  };
+
   render() {
-    // const { event } = this.props;
+    const { event } = this.props;
+    const { collapsed } = this.state;
     return (
       <div className='Event'>
-        <h2 className='summury'></h2>
-        <p className='start-time'></p>
-        <button className='details-button'>details</button>
+        <h2 className='summury'>{event.summary}</h2>
+        <p className='start-time'>{event.start.dateTime}</p>
+        <span className='location'>{event.location}</span>
+        <button className='details-button' onClick={this.toogleState}>
+          Details
+        </button>
+        {!collapsed && <EventDetails event={event} />}
       </div>
     );
   }
---
+++ src/EventDetails.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+class EventDetails extends Component {}
+
+export default EventDetails;
