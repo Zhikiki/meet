da55ba270b3f8f6b2efdbc160833761cd77488f7
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-02-19 20:02:35 +0100

    Comment to the branch

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+// THIS VERCION SHOULD RETURN ONE ERROR IN CONSOLE CONNECTED WITH () IN FETCH METHOD
+
 //  This function takes an events array, then uses map to create a new array with only locations.
 //  It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
 //  The Set will remove all duplicates from the array.
